e22844e18f41f5023d412629399b42eaa2ee9b14
Mischel <susan.mwaura@student.moringaschool.com>
2023-06-17 09:49:03 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
+
 function addingEventListener() {
-}
+    const input = document.getElementById('input');
+    const button = document.getElementById('button');
+  
+    function clickAlert() {
+      alert('I was clicked!');
+    }
+  
+    input.addEventListener('click', clickAlert);
+    button.addEventListener('click', clickAlert);
+  }
+  
+  addingEventListener(); // Call the function to bind the event listeners
+  
